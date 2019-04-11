cee5dec7fe725481f55e4593358cf8f90a0f3bcf
Leigh Baugham <leigh.baugham@gmail.com>
2019-04-11 12:07:15 -0400

    routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,35 @@
 class GamesController < ApplicationController
+    def index
+        @games = Game.all
+        render json: @games, status: :ok
+    end
+
+    def show 
+        @game = Game.find(params[:id])
+        render json: @game, status: :ok
+    end 
+    
+    def create 
+        @game = Game.create(game_params)
+        if @game
+            render json: @game, status: :ok
+        else
+            render json: {errors: @game.errors.full_messages},
+            status: :unprocessable_entity
+        end 
+    end 
+
+
+    def destroy
+        @game = Game.find(params[:id])
+        @game.destroy
+    end 
+
+    private
+
+    def game_params
+        params.permit(:user_id, :score, :word, :definition)
+    end 
+    
+end 
 end
